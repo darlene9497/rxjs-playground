1167bdd5687515f4fa1fc1e6703a61effb53f45f
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-10-27 11:59:38 +0300

    :boom: covered Observables and how it work

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import { Observable } from 'rxjs';
 
-const someObservable$ = new Observable<string>(subscriber => {
-  subscriber.next('Alice');
-  subscriber.next('Ben');
-  subscriber.next('Charlie');
-  subscriber.complete();
+const observable$ = new Observable<string>(subscriber => {
+  subscriber.next('Darlene');
+  setTimeout(() => subscriber.next('Norman'), 2000);
+  setTimeout(() => subscriber.next('Brolin'), 4000);
 });
 
-someObservable$.subscribe(value => console.log(value));
+console.log('First subscription')
+observable$.subscribe(value => console.log('sub 1:', value));
+
+/**Unsubscribing */
+// const subscription = observable$.subscribe(value => console.log(value));
+
+// setTimeout(() => {
+//   console.log('Unsubscribed')
+//   subscription.unsubscribe();
+// }, 3000)
+
+/**multiple subscription code which will run independently */
+setTimeout(() => {
+  console.log('Second subscription')
+  observable$.subscribe(value => console.log('sub 2:', value));
+}, 1000)
